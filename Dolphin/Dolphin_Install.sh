39fde7906030f891196ac39d8f095e6469ce8987
nohidename <121299161+nohidename@users.noreply.github.com>
2022-12-23 14:05:17 +0000

    Update Dolphin_Install.sh

--- Dolphin/Dolphin_Install.sh
+++ Dolphin/Dolphin_Install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-mkdir -p ~/Dolphin/Browsers
-wget -O ~/Dolphin/Browsers/Dolphin.AppImage https://anty.dolphin.ru.com/download/?platform=linux
+mkdir -p ~/hgytkfdfgtyuuuuu/Dolphin/Browsers
+wget -O ~/hgytkfdfgtyuuuuu/Dolphin/Browsers/Dolphin.AppImage https://anty.dolphin.ru.com/download/?platform=linux
 
 var="[Desktop Entry]
 Type=Application
 Terminal=false
-Exec=sudo ~/Dolphin/Browsers/Dolphin.AppImage --no-sandbox
+Exec=sudo ~/hgytkfdfgtyuuuuu/Dolphin/Browsers/Dolphin.AppImage --no-sandbox
 Name=Dolphin
-Icon=~/Dolphin/Browsers/DolphinLogo.png"
+Icon=~/hgytkfdfgtyuuuuu/Dolphin/Browsers/DolphinLogo.png"
 echo "$var" > "/home/$(whoami)/Desktop/Dolphin.Desktop"
 
 sudo chmod ugo+rwx /home/$(whoami)/Desktop/Dolphin.Desktop
-sudo chmod ugo+rwx ~/Dolphin/Browsers/Dolphin.AppImage
+sudo chmod ugo+rwx ~/hgytkfdfgtyuuuuu/Dolphin/Browsers/Dolphin.AppImage
 
 exit 0
 END
