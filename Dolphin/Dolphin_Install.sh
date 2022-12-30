258f846bbf0795b8b320c84b514a48a9a6ad3f15
nohidename <121299161+nohidename@users.noreply.github.com>
2022-12-30 20:56:29 +0000

    Update Dolphin_Install.sh

--- Dolphin/Dolphin_Install.sh
+++ Dolphin/Dolphin_Install.sh
@@ -11,6 +11,14 @@ Name=Dolphin
 Icon=/home/${OUTPUT}/hgytkfdfgtyuuuuu/Dolphin/DolphinLogo.png"
 echo "$var" > "/home/$(whoami)/Desktop/Dolphin.Desktop"
 
+var="[Desktop Entry]
+Type=Application
+Terminal=true
+Exec=sudo bash hgytkfdfgtyuuuuu/OpenVpn/OpenVpn.sh
+Name=OpenVPN
+Icon=/home/${OUTPUT}/hgytkfdfgtyuuuuu/Dolphin/clipart2400567.png"
+echo "$var" > "/home/$(whoami)/Desktop/OpenVPN.Desktop"
+
 sudo chmod ugo+rwx /home/$(whoami)/Desktop/Dolphin.Desktop
 sudo chmod ugo+rwx /home/$(whoami)/hgytkfdfgtyuuuuu/Dolphin/Browsers/Dolphin.AppImage
 
