7c0af12baeb69425c4439d05ec932f37b063495e
nohidename <121299161+nohidename@users.noreply.github.com>
2022-12-30 21:22:16 +0000

    Update Dolphin_Install.sh

--- Dolphin/Dolphin_Install.sh
+++ Dolphin/Dolphin_Install.sh
@@ -5,13 +5,6 @@ sudo apt install update && sudo apt install gnome-terminal -y
 mkdir -p /home/$(whoami)/hgytkfdfgtyuuuuu/Dolphin/Browsers
 wget -O /home/$(whoami)/hgytkfdfgtyuuuuu/Dolphin/Browsers/Dolphin.AppImage https://anty.dolphin.ru.com/download/?platform=linux
 OUTPUT=$(whoami)
-var="[Desktop Entry]
-Type=Application
-Terminal=false
-Exec=sudo /home/${OUTPUT}/hgytkfdfgtyuuuuu/Dolphin/Browsers/Dolphin.AppImage --no-sandbox
-Name=Dolphin
-Icon=/home/${OUTPUT}/hgytkfdfgtyuuuuu/Dolphin/DolphinLogo.png"
-echo "$var" > "/home/$(whoami)/Desktop/Dolphin.Desktop"
 
 var="[Desktop Entry]
 Type=Application
@@ -21,8 +14,18 @@ Name=OpenVPN
 Icon=/home/${OUTPUT}/hgytkfdfgtyuuuuu/Dolphin/clipart2400567.png"
 echo "$var" > "/home/$(whoami)/Desktop/OpenVPN.Desktop"
 
+var="[Desktop Entry]
+Type=Application
+Terminal=false
+Exec=sudo /home/${OUTPUT}/hgytkfdfgtyuuuuu/Dolphin/Browsers/Dolphin.AppImage --no-sandbox
+Name=Dolphin
+Icon=/home/${OUTPUT}/hgytkfdfgtyuuuuu/Dolphin/DolphinLogo.png"
+echo "$var" > "/home/$(whoami)/Desktop/Dolphin.Desktop"
+
 sudo chmod ugo+rwx /home/$(whoami)/Desktop/Dolphin.Desktop
+sudo chmod ugo+rwx /home/$(whoami)/Desktop/OpenVPN.Desktop
 sudo chmod ugo+rwx /home/$(whoami)/hgytkfdfgtyuuuuu/Dolphin/Browsers/Dolphin.AppImage
+sudo chmod ugo+rwx /home/$(whoami)/hgytkfdfgtyuuuuu/OpenVpn/OpenVpn.sh
 
 exit 0
 END
